c86a33cfca743e2ff2ad5010452fd5aab80f6c92
komanduk <102152679+komanduk@users.noreply.github.com>
2023-01-19 14:10:43 +0900

    [Bronze V] Title: 학점계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,29 @@
+Tmp = input()
+if Tmp == 'A+':
+    print(4.3)
+elif Tmp == 'A0':
+    print(4.0)
+elif Tmp == 'A-':
+    print(3.7)
+elif Tmp == 'A0':
+    print(4.0)
+elif Tmp == 'B+':
+    print(3.3)
+elif Tmp == 'B0':
+    print(3.0)
+elif Tmp == 'B-':
+    print(2.7)
+elif Tmp == 'C+':
+    print(2.3)
+elif Tmp == 'C0':
+    print(2.0)
+elif Tmp == 'C-':
+    print(1.7)
+elif Tmp == 'D+':
+    print(1.3)
+elif Tmp == 'D0':
+    print(1.0)
+elif Tmp == 'D-':
+    print(0.7)
+else:
+    print(0.0)
